8364cd48b9b38a17dcf613a46ee5b722aed4f2bf
Sonu Bhatt <itssonubhatt@gmail.com>
2024-07-12 20:36:57 +0530

    app autoopen turned off

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
 	optimizeDeps: {
 		include: ["browser-image-resizer", "uuid", "@xenova/transformers"],
 	},
-	server: {
-		open: "/",
-	},
+	// server: {
+	// 	open: "/",
+	// },
 });
